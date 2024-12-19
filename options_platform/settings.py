13726c37be66c7d4ca548335c98d02183b10c5f0
Orion-Artemis <omlande251220036@gmail.com>
2024-12-19 22:29:13 +0530

    Updated Port

--- options_platform/settings.py
+++ options_platform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rd_35c!%3ktr+^o5e5^s0$g^v(7y_*%%3vv0++-g9z4ozz6&t+'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+PORT = int(os.getenv("PORT", 8000))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['options-platform.onrender.com', '127.0.0.1', 'localhost']
-
 
 # Application definition
 
